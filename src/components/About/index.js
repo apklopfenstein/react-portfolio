823061d5a5f1ea4fc02fe2cb7d0ce69aa883cdcd
Alyssa Klopfenstein <alyssa.r.purdy@gmail.com>
2020-12-20 10:49:33 -0600

    style resume page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,11 +6,12 @@ function About() {
     return(
         <section id="about-me">
             <h2>About Me</h2>
+            <img src={portrait} alt="portrait" />
             <div>
                 <p>Hi, I'm Alyssa Klopfenstein! I'm currently a stay at home parent with 2 children who are are both about to be in school full time. I have a growing interest in web development and am hoping to use what I learn to reenter the work force. I'd also like to be able to co-create something with my husband.</p>
                 <p>I currently live in Texas and have 2 dogs. I love to watch my kids and husband play hockey. We're also big Columbus Blue Jacket fans! My favorite vacations are to the beach and I love pop punk music. It's nice to meet you!</p>
             </div>
-            <img src={portrait} alt="portrait" />
+            
         </section>
     );
 }
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Resume.css';
 
 function Resume() {
     return(
-        <div>
-            <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/e/2PACX-1vShJKGLbII2QF-vPRVjq7SSb2VtA_pSXJ7k6hVJAK4PKBB6sCZaS1vn5SeQ09lGbkV9cityLOEu-7Ou/pub">My Resume</a>
-            <ul>Front End Proficiencies
+        <>
+        <div className="skills">
+            <div className="front-end">
+            <h1>Front End Proficiencies</h1>
+            <ul>
                 <li>HTML</li>
                 <li>CSS</li>
                 <li>JavaScript</li>
@@ -13,14 +16,20 @@ function Resume() {
                 <li>Bootstrap</li>
                 <li>Responsive Design</li>
             </ul>
-            <ul>Back End Proficiencies
+            </div>
+            <div className="back-end">
+            <h1>Back End Proficiencies</h1>
+            <ul>
                 <li>APIs</li>
                 <li>Node</li>
                 <li>Express</li>
                 <li>MySQL/Sequelize</li>
                 <li>MongoDB</li>
             </ul>
+            </div>
         </div>
+        <a className="resume" target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/e/2PACX-1vShJKGLbII2QF-vPRVjq7SSb2VtA_pSXJ7k6hVJAK4PKBB6sCZaS1vn5SeQ09lGbkV9cityLOEu-7Ou/pub">My Resume</a>
+        </>
     )
 }
 
