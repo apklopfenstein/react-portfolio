26c95c4fb41cdb51de96860a0b27bececd5ab765
Alyssa Klopfenstein <alyssa.r.purdy@gmail.com>
2020-12-20 22:13:48 -0600

    make resume download pdf

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -29,7 +29,8 @@ function Resume() {
                     </ul>
                 </div>
             </div>
-            <Button className="resume" target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/e/2PACX-1vShJKGLbII2QF-vPRVjq7SSb2VtA_pSXJ7k6hVJAK4PKBB6sCZaS1vn5SeQ09lGbkV9cityLOEu-7Ou/pub">View My Resume</Button>
+            <Button className="resume" href="https://docs.google.com/document/d/1YaCfj7-VyA2lQiz14NTqH-l3gSrQwMHH-sIelYoIS8E/export?format=pdf
+            ">View My Resume</Button>
         </section>
     )
 }
